0812a9c795b2bfc5157e90159f385dfbc9c00750
abdiriiadan505 <abdiriiadan505@gmail.com>
2026-02-22 11:28:56 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,7 @@
-const CACHE_NAME = 'premium-calculator-v1';
-const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener('install', e => {
     self.skipWaiting();
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-    return self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+self.addEventListener('fetch', e => {
+    e.respondWith(fetch(e.request));
 });
